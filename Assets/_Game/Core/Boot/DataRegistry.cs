4fbe86fd1daacf52700fc92e085900864006abb1
tranngoc246 <tranvanngocyt@gmail.com>
2026-03-21 11:49:27 +0700

    refactor(data): move tower validation into partial DataRegistry file

--- Assets/_Game/Core/Boot/DataRegistry.cs
+++ Assets/_Game/Core/Boot/DataRegistry.cs
@@ -832,31 +832,3 @@ namespace SeasonalBastion
             return outArr;
         }
 
-        private void ValidateTowers()
-        {
-            foreach (var kv in _towers)
-            {
-                var id = kv.Key;
-                var d = kv.Value;
-
-                if (d.Rof <= 0f)
-                    _loadErrors.Add($"Tower '{id}': rof<=0 (shots/sec). Must be > 0.");
-
-                if (d.AmmoMax < 0)
-                    _loadErrors.Add($"Tower '{id}': ammoMax<0");
-
-                if (d.AmmoMax == 0 && d.AmmoPerShot != 0)
-                    _loadErrors.Add($"Tower '{id}': ammoMax=0 but ammoPerShot!=0 (expected 0).");
-
-                if (d.AmmoMax > 0 && d.AmmoPerShot <= 0)
-                    _loadErrors.Add($"Tower '{id}': ammoMax>0 but ammoPerShot<=0");
-
-                if (d.AmmoMax > 0 && d.AmmoPerShot > d.AmmoMax)
-                    _loadErrors.Add($"Tower '{id}': ammoPerShot({d.AmmoPerShot}) > ammoMax({d.AmmoMax})");
-
-                if (d.NeedsAmmoThresholdPct <= 0f || d.NeedsAmmoThresholdPct > 1f)
-                    _loadErrors.Add($"Tower '{id}': needsAmmoThresholdPct out of range (0..1]");
-            }
-        }
-    }
-}
